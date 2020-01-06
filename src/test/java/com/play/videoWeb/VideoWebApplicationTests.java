195098f152e2a6da41e743b06913fcad6e4be522
liujca <liujca@jiedaibao.com>
2020-01-06 16:42:12 +0800

    initial commit

---
+++ src/test/java/com/play/videoWeb/VideoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.play.videoWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
